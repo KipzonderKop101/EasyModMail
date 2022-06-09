cc12e38c96297cac8459bcc4b3f5d52cd8e5f5f2
KipZonderKop101 <80620930+KipzonderKop101@users.noreply.github.com>
2022-06-09 17:52:39 +0200

    WORK IN PROGESS - Added embeds, select menu and options

--- code/commands/help.js
+++ code/commands/help.js
@@ -1,18 +1,89 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageSelectMenu } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
-		.setDescription('See what you can do with this bot'),
+		.setDescription('See what you can do with this bot')
+        .addStringOption(option =>
+            option.setName('selection')
+                .setDescription('Select a help category')
+                .setRequired(false)
+                .addChoice('Thread', 'help_thread')
+                .addChoice('Reply', 'help_reply')
+                .addChoice('User', 'help_user')),
 	async execute(interaction) {
 		const generalHelpEmbed = new MessageEmbed()
-            .setColor('#21238a')
-            .setTitle('Courtroom help')
-            .setURL('https://github.com/KipzonderKop101/EasyModMail')
-            .setDescription("Choose what you'd like help on")
-            .setTimestamp();
+            .setAuthor({ name: 'EasyModMail', url: 'https://github.com/KipzonderKop101/EasyModMail'})
+            .addFields(
+                { name: 'Thread', value: '`/help thread`', inline: true },
+                { name: 'Replying', value: '`/help reply`', inline: true },
+                { name: 'User', value: '`/help user`', inline: true },
+            )
         
-        await interaction.reply({ embeds: [generalHelpEmbed] });
+        const threadHelpEmbed = new MessageEmbed()
+            .setTitle('Thread')
+            .setDescription('Useful commands for your threads')
+            .addFields(
+                { name: '`/delete [thread] (optional reason)`', value: 'Delete a thread', inline: false },
+                { name: '`/block [thread] (optional reason)`', value: 'Block a user from contacting mod-mail again', inline: false },
+                { name: '`/archive [thread] (optional reason)`', value: 'Archive a thread', inline: false },
+                { name: '`/transfer [thread] (optional reason)`', value: 'Transfer the thread to a different category', inline: false },
+                { name: '`/permissions [thread] [role] (optional reason)`', value: 'Change the permissions of a thread', inline: false},
+                { name: '`/close [thread] (optional message) (optional reason)`', value: 'Close a thread', inline: false}
+            )
+        
+        const replyHelpEmbed = new MessageEmbed()
+            .setTitle('Reply')
+            .setDescription('The different ways to reply to a threat')
+            .addFields(
+                { name: '`/reply [thread] [message]`', value: 'Reply to a thread', inline: false },
+                { name: '`/areply [thread] [message]`', value: 'Reply to a thread anonymously', inline: false },
+                { name: '`/snippets [thread] [snippet]`', value: 'Transfer the thread to a different category', inline: false },
+            )
+
+        const userHelpEmbed = new MessageEmbed()
+            .setTitle('User')
+            .setDescription('Different actions you can perform regarding the threat opener, the user')
+            .addFields(
+                { name: '`/info [thread]`', value: 'Info on the user that started the thread', inline: false },
+                { name: '`/kick [thread] (optional reason)`', value: 'Kick the user that started the thread', inline: false },
+                { name: '`/ban [thread] (optional reason)`', value: 'Ban the user that started the thread', inline: false },
+            )
+    
+        const selection = interaction.options.getString('selection');
+
+        if (selection === 'help_thread') {
+            await interaction.reply({ embeds: [threadHelpEmbed] });
+        } else if (selection === 'help_reply') {
+            await interaction.reply({ embeds: [replyHelpEmbed] });
+        } else if (selection === 'help_user') {
+            await interaction.reply({ embeds: [userHelpEmbed] });
+        } else {
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageSelectMenu()
+                        .setCustomId('helpMenu')
+                        .setPlaceholder('Make a selection')
+                        .addOptions([
+                            {
+                                label: 'Thread',
+                                description: 'Useful commands for your threads',
+                                value: 'select_threat',
+                            }, 
+                            {
+                                label: 'Reply',
+                                description: 'The different way to reply to a threat',
+                                value: 'select_reply',
+                            },
+                            {
+                                label: 'User',
+                                description: 'Different actions you can perform regarding the threat opener, the user',
+                                value: 'select_user'
+                            },
+                        ]),
+                );
+            await interaction.reply({ embeds: [generalHelpEmbed], components: [row] });
+        }
 	},
 };
